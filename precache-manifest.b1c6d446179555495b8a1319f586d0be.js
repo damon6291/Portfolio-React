7c45090f3f5eb53d23ec09e3cf9ef2a5162fd29c
damon6291 <damon@Sangmins-MacBook-Pro.local>
2020-04-19 03:59:27 -0400

    Updates

---
+++ precache-manifest.b1c6d446179555495b8a1319f586d0be.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470ba632588a5a19cdf024f1184b48ee",
+    "url": "/Portfolio.React/index.html"
+  },
+  {
+    "revision": "9637aaac93fb80e0843b",
+    "url": "/Portfolio.React/static/css/2.246d7c7d.chunk.css"
+  },
+  {
+    "revision": "ef86ca81147c021c89e2",
+    "url": "/Portfolio.React/static/css/main.deec5dc8.chunk.css"
+  },
+  {
+    "revision": "9637aaac93fb80e0843b",
+    "url": "/Portfolio.React/static/js/2.a6f47ea3.chunk.js"
+  },
+  {
+    "revision": "31210747bd639fc5a820c67e8f918966",
+    "url": "/Portfolio.React/static/js/2.a6f47ea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef86ca81147c021c89e2",
+    "url": "/Portfolio.React/static/js/main.035ee118.chunk.js"
+  },
+  {
+    "revision": "62b73a67e5587d39cf6e",
+    "url": "/Portfolio.React/static/js/runtime-main.d7ea868c.js"
+  },
+  {
+    "revision": "89ccac5365f50ee229092a997f5e62ca",
+    "url": "/Portfolio.React/static/media/background.89ccac53.jpg"
+  },
+  {
+    "revision": "c1df6887a94ccf06827f46fdaab36418",
+    "url": "/Portfolio.React/static/media/bg.c1df6887.jpg"
+  },
+  {
+    "revision": "1c1888a3eb2f04360763c1a95257b726",
+    "url": "/Portfolio.React/static/media/c.1c1888a3.jpg"
+  },
+  {
+    "revision": "e9588a3f5f390c210675b958db83cad4",
+    "url": "/Portfolio.React/static/media/collaboration.e9588a3f.jpg"
+  },
+  {
+    "revision": "24c26eb32a685ba3f55271166764c14e",
+    "url": "/Portfolio.React/static/media/corona.24c26eb3.jpg"
+  },
+  {
+    "revision": "c670df7f8b1b4d1795d6b4a0ac95e187",
+    "url": "/Portfolio.React/static/media/hacker.c670df7f.jpg"
+  },
+  {
+    "revision": "62be9fb6e1d7166e9cbeeed913096752",
+    "url": "/Portfolio.React/static/media/java.62be9fb6.jpg"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/Portfolio.React/static/media/javaScript.0cded3a3.jpg"
+  },
+  {
+    "revision": "36f20232ff76fec5d8cfcaacb1b3ac0e",
+    "url": "/Portfolio.React/static/media/movie.36f20232.jpg"
+  },
+  {
+    "revision": "cde43891991f76a5e7df17ac6d373aff",
+    "url": "/Portfolio.React/static/media/mysql.cde43891.jpg"
+  },
+  {
+    "revision": "df70b00589269cc7e903d0e3ba9db627",
+    "url": "/Portfolio.React/static/media/react.df70b005.jpg"
+  },
+  {
+    "revision": "b7c2e1329a5d1a5b47a2b980ada4ba0c",
+    "url": "/Portfolio.React/static/media/shell.b7c2e132.jpg"
+  },
+  {
+    "revision": "ff0892a3377ae0adc6d66ca1fa134534",
+    "url": "/Portfolio.React/static/media/vis.ff0892a3.jpg"
+  },
+  {
+    "revision": "2ece7c4025e98ddaeb544a47c52c1c34",
+    "url": "/Portfolio.React/static/media/weather.2ece7c40.jpg"
+  }
+]);
